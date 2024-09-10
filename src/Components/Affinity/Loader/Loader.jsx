b5f63a0b1847115dd9720f80817778582f76fb49
amir <cavalier7654@gmail.com>
2024-09-10 10:08:23 +0330

    preLoader

---
+++ src/Components/Affinity/Loader/Loader.jsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+	return(
+		<div className="h-screen w-screen flex justify-center items-center">
+			<img src="../../src/img/loader.gif" alt="" />
+		</div>
+	)
+}
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,26 +1,37 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Head from "./Components/Affinity/Header/Head";
 import Footer from "./Components/Affinity/Footer/Footer";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Loader from "./Components/Affinity/Loader/Loader";
 
 export default function Layout() {
+	const [preLoad, setPreLoad] = useState(true);
 	const { pathname } = useLocation();
 
 	useEffect(() => {
-		window.scrollTo(0 , 0)
-	} , [pathname])
+		window.scrollTo(0, 0);
+		window.onload = () => {
+			setPreLoad(false);
+		};
+	}, [pathname]);
 
 	return (
 		<div>
-			<div className={`container max-w-maxWidth`}>
-				<Head />
-			</div>
+			{preLoad ? (
+				<Loader />
+			) : (
+				<>
+					<div className={`container max-w-maxWidth`}>
+						<Head />
+					</div>
 
-			<Outlet />
+					<Outlet />
 
-			<div className={`mt-20`}>
-				<Footer />
-			</div>
+					<div className={`mt-20`}>
+						<Footer />
+					</div>
+				</>
+			)}
 		</div>
 	);
 }
